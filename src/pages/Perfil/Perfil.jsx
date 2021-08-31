be59c8049f7f0fb5467400b27a4002a27c38d6b4
Pedro Medeiros <81324921+pedro21medeiros@users.noreply.github.com>
2021-08-31 14:12:13 -0300

    implementa pagina Perfil, com a logica de settar o email na pagina de forma fixa. Tambem remove do localStorage e redireciona para a pagina "/" (#687)

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 import HeaderWithoutSearch from '../../components/Header/HeaderWithoutSearch';
 import Footer from '../../components/Footer/Footer';
+import getLocalStorage from '../../services/localStorage/getLocalStorage';
 
-const Perfil = () => (
-  <div>
-    <HeaderWithoutSearch title="Perfil" />
-    <Footer />
-  </div>
+function Perfil() {
+  const [email, setEmail] = useState('');
+  const history = useHistory();
 
-);
+  useEffect(() => {
+    const handleEmail = () => {
+      const userEmail = getLocalStorage('user') || { email: '' };
+      setEmail(userEmail.email);
+    };
+    handleEmail();
+  }, []);
+
+  const handleRouteLogin = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <div>
+      <HeaderWithoutSearch title="Perfil" />
+      <h3 data-testid="profile-email">{email}</h3>
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleRouteLogin }
+      >
+        Sair
+      </button>
+      <Footer />
+    </div>
+  );
+}
 
 export default Perfil;
